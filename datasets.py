f7ca96fe32f835b6bc403f1df4fd24b33af30578
PigletOS <37327442+PigletOS@users.noreply.github.com>
2019-05-11 17:29:18 +0100

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -40,6 +40,7 @@ def find_classes(dir):
 def make_dataset(dir, class_to_idx):
   images = []
   dir = os.path.expanduser(dir)
+  print('data path is : 'dir)
   for target in tqdm(sorted(os.listdir(dir))):
     d = os.path.join(dir, target)
     if not os.path.isdir(d):
