5cc7100df9888fdab2c1b509aeda5260d6396c39
PigletOS <37327442+PigletOS@users.noreply.github.com>
2019-05-13 01:20:06 +0100

    Update prepare_data.sh

--- scripts/utils/prepare_data.sh
+++ scripts/utils/prepare_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python make_hdf5.py --dataset I128 --batch_size 256 --data_root data
-python calculate_inception_moments.py --dataset I128_hdf5 --data_root data
+python make_hdf5.py --dataset I64 --batch_size 10 --data_root data
+python calculate_inception_moments.py --dataset I64_hdf5 --data_root data
